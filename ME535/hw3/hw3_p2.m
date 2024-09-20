92c8e39046f30d96de9f14d59f91a3eb28b313e1
Joseph <joeyscarter@gmail.com>
2024-09-19 23:13:19 -0600

    got these problems working

---
+++ ME535/hw3/hw3_p2.m
@@ -0,0 +1,54 @@
+clear
+clc
+close all
+
+dr = linspace(0,1,100);
+
+for i = 1:length(dr)
+    m = 1;
+    natural_frequency = 5 * 2 * pi;
+    k = (natural_frequency^2) * m;
+    v = 5;
+    g = 9.8;
+    damping_ratio = dr(i);
+    c = damping_ratio * 2 * sqrt(k*m);
+    t = linspace(0,2,300);
+    z_0 = 0;
+    z_dot_0 = v;
+    
+    z_func = @(t) exp(-damping_ratio * natural_frequency * t) + (v/(natural_frequency * sqrt(1 - damping_ratio^2))) * sin(natural_frequency * sqrt(1 - damping_ratio^2) * t);
+    
+    z = exp(-damping_ratio .* natural_frequency .* t) .* (v./(natural_frequency .* sqrt(1 - damping_ratio.^2))) .* sin(natural_frequency .* sqrt(1 - damping_ratio.^2) .* t);
+    
+    % hold on
+    % plot(t,z)
+    
+    [tout,yout] = ode45(@(t, z) eom_2_12(z, m, k, c, g), [t(1),t(end)],[z_0; z_dot_0]);
+    
+    % plot(tout,yout(:,1))
+    % ylabel('z (m)','interpreter','latex')
+    % xlabel('time (s)','interpreter','latex')
+    
+    sum_force = m * g - (yout(:,2) * c + k * yout(:,1));
+    sum_force_abs = abs(sum_force);
+    max_abs_force(i) = max(sum_force_abs);
+
+    % hold on
+    % plot(tout,sum_force)
+end
+
+[minimum, idx] = min(max_abs_force);
+best_damping_ratio = dr(idx);
+
+hold on
+plot(dr,max_abs_force)
+
+function [z_dot] = eom_2_12(z, m, k, c, g)    
+    % Forcing - sum of step and ramp
+    % F = 0;
+    F = m * g;
+    
+    % Equations of Motion
+    z_dot(1,1) = z(2);
+    z_dot(2,1) = (1/m) * (F - c * z(2) - k * z(1));
+end
--- ME535/hw3/hw3_p5.m
+++ ME535/hw3/hw3_p5.m
@@ -2,34 +2,45 @@ clear
 clc
 close all
 
+%unit system: mm g s N
+
 L = 2e-2;
 m = 3e-3;
 kappa = 20;
-damping_ratio = 0.02;
+damping_ratio = 0.0;
 angle_deg = 30;
 F0 = kappa * deg2rad(angle_deg) / L;
 q_0 = deg2rad(angle_deg);
 q_dot_0 = 0;
-natural_frequency = sqrt(kappa / (1/3 * m * L^2));
+natural_frequency = sqrt(kappa / m);
 damped_frequency = natural_frequency * sqrt(1 - damping_ratio^2);
 damped_period = 2 * pi / damped_frequency;
 T = 2.5 * damped_period;
 c = damping_ratio * 2 * sqrt(kappa*m);
 
-ts = linspace(0,10*T,100);
+ts = linspace(0,10*T,725);
+% ts = linspace(0,0.1,100);
+
+step_solution = @(t) (1./(m.*natural_frequency.^2)) .* (1 - exp(-damping_ratio .* natural_frequency .* t) .* cos(damped_frequency .* t) + (damping_ratio .* natural_frequency ./ damped_frequency) .* sin(damped_frequency .* t));
+
+q_analytic = exp(-damping_ratio .* natural_frequency .* ts) .* (q_0 .* cos(damped_frequency .* ts) + (q_dot_0 + damping_ratio .* natural_frequency .* q_0 ./ damped_frequency) .* sin(damped_frequency .* ts));
+q_f_analytic = F0 .* (step_solution(ts) .* heaviside(ts) - step_solution(ts - T) .* heaviside(ts - T));
+hold on
+plot(ts,q_f_analytic + q_analytic,'black')
 
 [tout,yout] = ode45(@(t, theta) eom_2_12(t, theta, L, m, kappa, damping_ratio, F0, natural_frequency, damped_frequency, T, c), [ts(1),ts(end)],[q_0; q_dot_0]);
 
-plot(tout,yout(:,1))
+plot(tout,yout(:,1),'red')
 ylabel('${\theta}$ (rad)','interpreter','latex')
 xlabel('time (s)','interpreter','latex')
 
 function [theta_dot] = eom_2_12(t, theta, L, m, kappa, damping_ratio, F0, natural_frequency, damped_frequency, T, c)    
     % Forcing - sum of step and ramp
-    F = F0 * (t < T && t > 0);
+    F = F0 * (heaviside(t) - heaviside(t - T));
     % F = 0;
     
     % Equations of Motion
     theta_dot(1,1) = theta(2);
-    theta_dot(2,1) = (3/(m*L^2)) * (F - c * theta(2) - kappa * theta(1));
+    % theta_dot(2,1) = ((1/3) * m * L^2)^(-1) * (F - c * theta(2) - kappa * theta(1));
+    theta_dot(2,1) = (1/m) * (F - c * theta(2) - kappa * theta(1));
 end
