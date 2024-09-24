53e580d3cd15b98b59b1e120730e84843add136d
Joseph <joeyscarter@gmail.com>
2024-09-24 17:11:28 -0600

    update

--- ME535/hw3/hw3_p3.m
+++ ME535/hw3/hw3_p3.m
@@ -5,8 +5,8 @@ close all
 F0 = 1;
 beta = 0.1;
 T = 5;
-t = linspace(0,10,100);
-exponential = exp(-beta * (t - 2*T)) - F0;
+t = linspace(0,10,1000);
+exponential = F0 .* heaviside(t) - F0 .* heaviside(t - T) + F0 .* exp(-beta .* (t - T)) .* heaviside(t - T);
 combined = t;
 combined(t < T) = F0;
 combined(t > T) = F0 * exp(-beta * (t(t > T) - T));
