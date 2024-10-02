8bf97be23600d41537c5820b7e49e43a4d97d43a
Joseph <joeyscarter@gmail.com>
2024-10-01 18:10:42 -0600

    cant remember

--- ME535/hw4/hw_p5.m
+++ ME535/hw4/hw_p5.m
@@ -4,14 +4,13 @@ close all
 
 m1 = 0.5;
 m2 = 1;
-k1 = 3.2e3;
-k2 = 0;
-mu1 = 0;
-mu2 = 40;
+k = 3.2e3;
+mu = 40;
 A = 20e-3;
 omega = 75;
 
-F = (-A * 1i) * (k2 + 1i * omega * mu2 - (m1+m2) * omega^2);
+a = k - m1 * omega^2;
+b = 1i*omega*mu - m2*omega^2;
+F = -A*1i/(a/b);
 
 abs(F)
-atan2(imag(F)/real(F),1)
