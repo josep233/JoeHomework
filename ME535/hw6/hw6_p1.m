ecb54dd1a8e764c14f29a2e9df9fc2b172ff4a47
Joseph <joeyscarter@gmail.com>
2024-10-16 21:46:02 -0600

    update

--- ME535/hw6/hw6_p1.m
+++ ME535/hw6/hw6_p1.m
@@ -6,24 +6,24 @@ l = 1;
 k = 1000;
 m = 1;
 theta = deg2rad(45);
-xs = linspace(-1,1,100);
+xs = linspace(-1,1,10);
 
 syms X L THETA M K
 
-L_i = L / cos(THETA);
-h = sqrt((L^2 / cos(THETA)^2) - L^2);
+h = L * tan(THETA);
+L_i = sqrt(h^2 + L^2);
 L_f = sqrt(h^2 + (L + X)^2);
 delta = L_f - L_i;
 delta_squared = simplify(delta^2);
 
 potential_energy = (1/2) * K * delta_squared;
-K_matrix = simplify(diff(diff(potential_energy,X),X));
+K_matrix = simplify(diff(potential_energy,X,2));
 
 K_full = subs(K_matrix,{L,K,THETA}, {l, k, theta});
 
-K_taylor = taylor(K_matrix,X);
+K_taylor = taylor(K_matrix,X,order=2);
 
-% pretty(K_taylor)
+pretty(K_taylor)
 
 K_of_X = subs(K_taylor,{L,K,THETA}, {l, k, theta});
 
@@ -35,17 +35,19 @@ t_range = [0 1];
 x_0_1 = 0.1;
 x_0_2 = 0.5;
 
-% [tout1,yout1] = ode45(@(t, x) eom(t, x, K_full, m), t_range, [x_0_1, 0]);
-% [tout2,yout2] = ode45(@(t, x) eom(t, x, K_full, m), t_range, [x_0_2, 0]);
+[tout1,yout1] = ode45(@(t, x) eom(t, x, K_full, m), t_range, [x_0_1, 0]);
+[tout2,yout2] = ode45(@(t, x) eom(t, x, K_full, m), t_range, [x_0_2, 0]);
 
-hold on
-% plot(xs,k_of_x,'blue')
-plot(xs,sqrt(k_of_x/m),'green')
-% yline(k * cos(theta)^2,'red')
+nat_1 = 2* pi / double(sqrt(subs(K_of_X,X,0)/m));
 
 % hold on
-% plot(tout1,yout1(:,2))
-% plot(tout2,yout2(:,2))
+% plot(xs,k_of_x,'blue')
+% plot(xs,sqrt(k_of_x/m),'green')
+% yline(k * cos(theta)^2,'red')
+
+hold on
+plot(tout1,yout1(:,1))
+plot(tout2,yout2(:,1))
 
 function [theta_dot] = eom(t, x, k_func, m)        
     % Equations of Motion
